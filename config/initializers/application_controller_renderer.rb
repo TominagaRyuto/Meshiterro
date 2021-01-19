895d8264196fab66aa3cf176548e03a7c244a234
TominagaRyuto <esouunnpou@gmail.com>
2021-01-19 11:11:04 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1b7517e005af14b977b86b24ad60ba940d87b2f5275c7e25eb3ee033cf0385992c798c3404eece3587e3da9e7ad6729f6cbaae4e11eb2640fdbbb00c7cf3ad87'
